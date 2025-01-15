53b01b871993097ef4466ee9ecbd936db78fe722
4rge/michina <93495092+4rge@users.noreply.github.com>
2025-01-15 01:07:55 -0800

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,24 +1,4 @@
 function serve() {
-    python3 -c "
-import http.server
-import socketserver
-import webbrowser
-import os
-
-PORT=8080
-
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        super().end_headers()
-
-def open_browser():
-    webbrowser.open_new(f'http://localhost:{PORT}')
-
-if __name__ == '__main__':
-    with socketserver.TCPServer(('', PORT), Handler) as httpd:
-        print(f'Serving at http://localhost:{PORT}')
-        open_browser()
-        httpd.serve_forever()
-"
+    python3 -m http.server 8080 --bind 127.0.0.1 --directory . &
+    sleep 1 && xdg-open "http://localhost:8080"
 }
